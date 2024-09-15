3951a408be6a33eae30ea8664bce46bea08d98e0
Soukaina Jabri <soukaina.jabri333@gmail.com>
2024-09-15 14:18:27 +0100

    Adding routes guard and implementing the logout

--- book-network-ui/src/app/services/token/token.service.ts
+++ book-network-ui/src/app/services/token/token.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {JwtHelperService} from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,26 @@ export class TokenService {
 
   get token() {
     return localStorage.getItem('token') as string;
+  }
+
+  isTokenNotValid() {
+    return !this.isTokenValid();
+  }
+
+  private isTokenValid() {
+    const token = this.token;
+    if (!token) { // if we don't have a token
+      return false;
+    }
+
+    // --------- decode the token ----------
+    const jwtHelper = new JwtHelperService();
+    // check the expiry date
+    const isTokenExpired = jwtHelper.isTokenExpired(token);
+    if (isTokenExpired) {
+      localStorage.clear();
+      return false;
+    }
+    return true;
   }
 }
